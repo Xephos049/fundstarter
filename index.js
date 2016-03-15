b339730546294bb55ed83bf580f014713eb53c52
xephos049 <joseph.joe93@gmail.com>
2016-03-15 18:59:38 +0000

    1b complete

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+
 var http = require('http');
 var fs = require('fs');
 
+
+
 http.createServer(function (request, response) {
-	response.end(fs.readFileSync(__dirname + '/fundstarter.html'),'utf8');
+	fs.readFile('fundstarter.html', function(error, content) {
+		if (error) {
+			response.writeHead(500);
+			response.end();
+		}
+		else {
+			response.writeHead(200, { 'Content-Type': 'text/html' });
+			response.end(content, 'utf-8');
+		}
+	});
 }).listen(process.env.PORT || 8080);
 
