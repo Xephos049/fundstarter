cbbf4155fbaf4e8bbd8c221471640bd9356587eb
xephos049 <joseph.joe93@gmail.com>
2016-03-15 18:56:21 +0000

    1a complete and working

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ var fs = require('fs');
 
 http.createServer(function (request, response) {
 	response.end(fs.readFileSync(__dirname + '/fundstarter.html'),'utf8');
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
 
-console.log('Server running on port: 8080');
