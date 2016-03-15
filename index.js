a3d6d1f6af2dcfe2f050bc3f157b409d50de926b
Ubuntu <ubuntu@ip-172-31-40-131.us-west-2.compute.internal>
2016-03-15 05:27:20 +0000

    2 complete

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response) {
-	fs.readFile('fundstarter.html', function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-		}
-		else {
-			response.writeHead(200, { 'Content-Type': 'text/html' });
-			response.end(content, 'utf-8');
-		}
-	});
+fs.open('fundstarter.html', 'r', function(err, fd) {
+    fs.fstat(fd, function(err, stats) {
+      var bufferSize=stats.size,
+            chunkSize=512,
+            buffer=new Buffer(bufferSize),
+            bytesRead = 0;
+
+        while (bytesRead < bufferSize) {
+            if ((bytesRead + chunkSize) > bufferSize) {
+               chunkSize = (bufferSize - bytesRead);
+           }
+            fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
+            bytesRead += chunkSize;
+        }
+        //console.log(buffer.toString('utf8', 0, bufferSize));
+        buffer.toString('utf8',0,bufferSize);
+	response.end(buffer);
+	fs.close(fd);
+    });
+});
 }).listen(8080);
 
 console.log('Server running on port: 8080');
