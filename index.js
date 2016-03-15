689a877b3d0590eb6f9c593fc6009d10ef0ced16
Ubuntu <ubuntu@ip-172-31-40-131.us-west-2.compute.internal>
2016-03-15 05:34:21 +0000

    1a complete

--- index.js
+++ index.js
@@ -2,26 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response) {
-fs.open('fundstarter.html', 'r', function(err, fd) {
-    fs.fstat(fd, function(err, stats) {
-      var bufferSize=stats.size,
-            chunkSize=512,
-            buffer=new Buffer(bufferSize),
-            bytesRead = 0;
-
-        while (bytesRead < bufferSize) {
-            if ((bytesRead + chunkSize) > bufferSize) {
-               chunkSize = (bufferSize - bytesRead);
-           }
-            fs.read(fd, buffer, bytesRead, chunkSize, bytesRead);
-            bytesRead += chunkSize;
-        }
-        //console.log(buffer.toString('utf8', 0, bufferSize));
-        buffer.toString('utf8',0,bufferSize);
-	response.end(buffer);
-	fs.close(fd);
-    });
-});
+	response.end(fs.readFileSync(__dirname + '/fundstarter.html'),'utf8');
 }).listen(8080);
 
 console.log('Server running on port: 8080');
